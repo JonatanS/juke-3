90f8a946154d98797a4c75c8c9134a59e5bff7c5
Yustynn <yustynn@gmail.com>
2015-12-04 16:44:01 -0500

    Finish album state

--- browser/js/states/states.js
+++ browser/js/states/states.js
@@ -1,11 +1,22 @@
 
 app.config(function ($stateProvider) {
-	$stateProvider.state('allAlbums', {
+
+	// Album states
+	$stateProvider.state('albums', {
         templateUrl: '/templates/albums.html',
         controller: 'AlbumsCtrl',
         url: '/albums'
 	});
-	$stateProvider.state('allArtists', {
+
+	$stateProvider.state('album', {
+		templateUrl: '/templates/album.html',
+		controller: 'AlbumCtrl',
+		url: '/albums/:id'
+	})
+
+
+
+	$stateProvider.state('artists', {
 		templateUrl: '/templates/artists.html',
 		controller: 'ArtistsCtrl',
 		url: '/artists'
